2690ee190dfbdabdd693bb73c680d377d4c710ac
ScottyHall <scotty.hall.email@gmail.com>
2018-06-14 21:56:45 -0600

    initial commit to auth branch

--- greySky.py
+++ greySky.py
@@ -3,6 +3,98 @@ import tkinter
 from PIL import ImageTk, Image
 
 
+class Person(object):
+    """general person building class to set, edit, return attributes
+
+    Attributes:
+    firstName: string
+    lastName: string
+    age: int 1 - 100
+    background: long string
+    gender: m/f
+    strength: 0 - 100
+    health: 1 - 100
+    infected status: 0/1
+    inventory: list
+    current room: room object
+    """
+
+    def __init__(self, firstName, lastName, age,
+                 background, gender, strength,
+                 health, infectedStatus):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.age = age
+        self.background = background
+        self.gender = gender
+        self.strength = strength
+        self.health = health
+        self.infectedStatus = infectedStatus
+        self.inventory = []
+        self.currentRoom = ""
+
+    # returns
+
+    def return_first(self):
+        return self.firstName
+
+    def return_last(self):
+        return self.lastName
+
+    def return_age(self):
+        return self.age
+
+    def return_background(self):
+        return self.background
+
+    def return_gender(self):
+        return self.gender
+
+    def return_strength(self):
+        return self.strength
+
+    def return_health(self):
+        return self.health
+
+    def return_infected(self):
+        return self.infectedStatus
+
+    def return_inventory(self):
+        return self.inventory
+
+    def return_currentroom(self):
+        return self.currentRoom
+
+    # setters
+
+    def set_first(self, first):
+        self.firstName = first
+
+    def set_last(self, last):
+        self.lastName = last
+
+    def set_age(self, age):
+        self.age = age
+
+    def set_background(self, bg):
+        self.background = bg
+
+    def set_gender(self, gender):
+        self.gender = gender
+
+    def set_strength(self, strength):
+        self.strength = strength
+
+    def set_health(self, health):
+        self.health = health
+
+    def set_infectedStatus(self, infected):
+        self.infectedStatus = infected
+
+    def set_currentRoom(self, room):
+        self.currentRoom = room
+
+
 def doNothing():
     print("do nothing triggered")
 
@@ -63,10 +155,11 @@ blackline = displayCanvas.create_line(3, 4, 250, 50)
 redline = displayCanvas.create_line(200, 2, 20, 120, fill='red')
 greenBox = displayCanvas.create_rectangle(40, 40, 120, 100, fill='green')
 
-# displayCanvas.delete(redline)	#delete line
-# displayCanvas.delete(ALL)	#delete all from canvas
+# displayCanvas.delete(redline) #delete line
+# displayCanvas.delete(ALL) #delete all from canvas
 
-saveImg = ImageTk.PhotoImage(Image.open("save.png"))
+saveIcon = "images/save.png"  # file location for icons
+saveImg = ImageTk.PhotoImage(Image.open(saveIcon))
 displayCanvas.create_image(10, 10, anchor=tkinter.NW,
                            image=saveImg, tags='image1')
 displayCanvas.create_image(80, 80, anchor=tkinter.NW,
